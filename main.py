d4f273a8dd67dd2ea6f5b1944c478c480990588d
mkortala <25817784+mkortala@users.noreply.github.com>
2020-04-04 15:50:55 +0200

    src/alg

--- main.py
+++
@@ -1,5 +0,0 @@
-from data_reader import read_data
-
-path = './Ant-Colony-CVRP/data_set_1.xlsx'
-
-example_instance = read_data(path)
---
+++ src/main.py
@@ -0,0 +1,8 @@
+from data_reader import read_data
+
+path = './data_set_1.xlsx'
+
+example_instance = read_data(path)
+
+print(example_instance.demands)
+print(example_instance.costs)
